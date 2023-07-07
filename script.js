92dda36777e73ac2ef94ffe1cf744a03c5f33e5a
29Kaggia <135098958+29Kaggia@users.noreply.github.com>
2023-07-07 09:09:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-//fetch film data
-const fetchFilms = () => {
-    fetch('${http://localhost:3000/films}', {
-        method: `GET`,
-        headers: {
-            "content-Type" : "application/json",
-        }
-    })
-
-.then((response) => response.json()
-.then(json => {
-    json.map(film => {
-        console.log(film)
-    })
+document.addEventListener('DOMContentLoaded', function(){
+    fetchData()
 })
-)
+//to fetch data from json file
+function fetchData(){
+    fetch(`http://localhost:3000/films`)
+    .then(res => res.json())
+    .then(data => {
+        displayTitles(data)
+    })
 }
 
-//Request to receive first movie data
-fetch('${http://localhost:3000/films/1}')
-.then(response => response.json())
-.then(movieData => {
-    const availableTickets = movieData.capacity-movieData.tickets
-})
+//to display movie titles
+function displayTitles(movie){
+    let titles = document.getElementById('films')
+    movie.forEach(movie =>{
+        let titleList = document.createElement('li')
+        titleList.classList = 'movie_list'
+        titleList.innerText = movie.title
+        titles.appendChild(titleList)
+        titleList.addEventListener('click', function(){
+            fetchInfo(movie)
+        })
+    })
+}
 
-//display movie details
-const posterImage = document.getElementById('poster-img');
-const movieTitle = document.getElementById('movie-title');
-const movieRuntime = document.getElementById('movie-runtime');
-const movieShowtime = document.getElementById('movie-showtime');
-const movieTicket = document.getElementById('movie-ticket');
+//to fetch information about the movie
+function fetchInfo(movie){
+    fetch(`http://localhost:3000/films/${movie.id}`)
+    .then(res => res.json())
+    .then(data => {
+        displayInfo(data)
+    })
+}
+
+function displayInfo(movie){
+    document.querySelector('#title').innerText = movie.title
+    document.querySelector('#description').innerText = `Description: ${movie.description}`
+    document.querySelector('#runtime span').innerText = `Runtime: ${movie.runtime}`
+    document.querySelector('#capacity span').innerText = `Capacity: ${movie.capacity}`
+    document.querySelector('#showtime span').innerText = `Showtime: ${movie.showtime}`
+    document.querySelector('#tickets span').innerText = `Tickets: ${movie.tickets_sold}`
+    document.querySelector('#images').setAttribute('src', movie.poster)
+    let btn = document.getElementById('buy_ticket')
+    btn.addEventListener('click', buyTicket)
+}
+
+function buyTicket(){
+    let tickets= document.getElementById('available');
+    let availableTickets = parseInt(tickets.textContent.split(' ')[2]);
+    if (availableTickets > 0) {
+        availableTickets--;
+        tickets.textContent = `Available Tickets: ${availableTickets}`;
+      } else {
+        alert('Sorry, the showing is sold out!')
+      }
+    }
